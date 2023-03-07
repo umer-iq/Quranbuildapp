0e9dac1100bc9438563aa87c75a3a1ec87ad4a98
umer <umeritsolutions@gmail.com>
2023-03-07 15:55:47 +0500

    add

---
+++ constants/images.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native";
+//export const otherWordsForHome = require("../assets/images/other_words_for_home.jpg");
+//export const theMetropolist = require("../assets/images/the_metropolist.jpg");
+//export const theTinyDragon = require("../assets/images/the_tiny_dragon.jpg");
+//export const underland = require("../assets/images/underland.jpg");
+export const yaseen = require('../assets/yaseen/yaseenlogo.png')
+export const Manzillogo = require('../assets/manzilfront.jpg')
+//export const Quranaudio = require('../assets/audioquran.jpg')
+export const Albakra = require('../assets/baqrah.webp')
+export const salah = require('../assets/praylogo.jpg')
+//export const namaz = require('../assets/namazkatareeqaurdu.jpg')
+export const kalma = require('../assets/kalma6.png')
+export const adan = require('../assets/azan.png')
+export const islam = require('../assets/Towards-Understanding-Islam-1.jpg')
+export const pillar = require('../assets/pillars.jpg')
+export const fast = require('../assets/fasting.jpg')
+export const Quran = require('../assets/reasons_to_read_quran.webp')
+//export const Kaba = require('../assets/kaba.jpg')
+//export const Kul = require('../assets/4kul.jpg')
+export const Kaba1 = require('../assets/kaba1.jpg')
+//export const Kabadoor = require('../assets/kabadoor.jpg')
+export const eid = require('../assets/eidsalah.jpg')
+
+export default {
+    
+    fast,Quran,
+    
+    
+    yaseen,Manzillogo,salah,Albakra,kalma,adan,islam,pillar,Kaba1,eid
+}
+const styles = StyleSheet.create({
+    salah: {
+    // flex: 1,
+    //  alignItems: 'center',
+    // justifyContent: 'center',
+    //  backgroundColor: "black",
+    width:100
+     
+     
+     
+    }})
